ace220193ffe86fc221f38cc31e899fa73e501bd
Fang1217 <57334019+Fang1217@users.noreply.github.com>
2025-09-23 02:46:13 +0800

    feat: connect to strapi BE

---
+++ src/app/article/[id]/page.tsx
@@ -0,0 +1,46 @@
+import { strapi } from "@strapi/client";
+
+import Header from "@/components/common/header";
+import SiteContent from "@/components/common/site-content";
+import Footer from "@/components/common/footer";
+import { Card, CardContent } from "@/components/ui/card";
+
+export default async function Article({ params }: { params: { id: string } }) {
+  const documentId = params.id;
+
+  if (!process.env.STRAPI_API_URL || !process.env.STRAPI_API_TOKEN) {
+    throw new Error(
+      "STRAPI_API_URL and STRAPI_API_TOKEN must be defined in environment variables."
+    );
+  }
+
+  const client = strapi({
+    baseURL: process.env.STRAPI_API_URL as string,
+    auth: process.env.STRAPI_API_TOKEN as string,
+  });
+
+  const articles = client.collection("articles");
+
+  try {
+    const article = await articles.findOne(documentId);
+    return (
+      <main>
+        <section>
+          <Header />
+          <SiteContent>
+            <Card>
+              <CardContent>
+                <h2>{article.data.title}</h2>
+                <div>{article.data.content}</div>
+              </CardContent>
+            </Card>
+          </SiteContent>
+          <Footer />
+        </section>
+      </main>
+    );
+  } catch (error) {
+    console.error("Error fetching article:", error);
+    return <div>Error loading article.</div>;
+  }
+}
---
+++ src/app/article/page.tsx
@@ -0,0 +1,43 @@
+import Footer from "@/components/common/footer";
+import Header from "@/components/common/header";
+import SiteContent from "@/components/common/site-content";
+import { strapi } from "@strapi/client";
+// Documentation: https://docs.strapi.io/cms/api/client
+
+if (!process.env.STRAPI_API_URL || !process.env.STRAPI_API_TOKEN) {
+  throw new Error(
+    "STRAPI_API_URL and STRAPI_API_TOKEN must be defined in environment variables."
+  );
+}
+
+const client = strapi({
+  baseURL: process.env.STRAPI_API_URL as string,
+  auth: process.env.STRAPI_API_TOKEN as string,
+});
+
+export default async function CMSPage() {
+  const articles = client.collection("articles");
+
+  // Fetch all english articles sorted by title
+  const allArticles = await articles.find({
+    // locale: "en",
+    sort: "title",
+  });
+
+  return (
+    <html>
+      <Header />
+      <SiteContent>
+        <h1>CMS Articles</h1>
+        <ul>
+          {allArticles.data.map((article) => (
+            <li key={article.documentId}>
+              <a href={`/article/${article.documentId}`}>{article.title}</a>
+            </li>
+          ))}
+        </ul>
+      </SiteContent>
+      <Footer />
+    </html>
+  );
+}
